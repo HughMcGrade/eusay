3d3c5dec32d8cae2e04bd78e69626e0d01046bff
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2014-02-18 19:29:09 +0000

    Add models and test views

--- eusay/models.py
+++ eusay/models.py
@@ -8,12 +8,37 @@ from django.db import models
 class Comment (models.Model):
     id = models.AutoField(primary_key=True)
     text = models.CharField(max_length=500)
-    user = models.ForeignKey() #TODO
-    proposal = models.ForeignKey()
     date = models.DateField()
+    user = models.ForeignKey("User")#, related_name="votedForComment")
+
+class Vote (models.Model):
+    id = models.AutoField(primary_key=True)
+    isVoteUp = models.BooleanField()
+    date = models.DateField()
+    user = models.ForeignKey("User")#, related_name="votedFor")
+    
+class Proposal (models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    actionDescription = models.CharField(max_length=2000)
+    backgroundDescription = models.CharField(max_length=2000)
+    beliefsDescription = models.CharField(max_length=2000)
+    votes = models.ForeignKey(Vote, null=True, blank=True)
+    comments = models.ForeignKey(Comment, null=True, blank=True)
+    proposer = models.ForeignKey("User")#, related_name="proposed")
+    
+class ProposalVote (Vote):
+    proposal = models.ForeignKey(Proposal, null=True)
+    
+class CommentVote (Vote):
+    comment = models.ForeignKey(Comment, null=True)
     
 class User (models.Model):
-    id = models.AutoField(primary_key=True)
+    sid = models.CharField(max_length=20, primary_key=True)
     name = models.CharField(max_length=50)
-    
+    votedFor = models.ForeignKey(Vote, null=True, blank=True, related_name="Vote.user")
+    commented = models.ForeignKey(Comment, null=True, blank=True, related_name="Comment.user")
+    proposed = models.ForeignKey(Proposal, null=True, blank=True, related_name="Proposal.proposer")
+    signUpDate = models.DateField()
+    candidateStatus = models.CharField(max_length=20)
     
