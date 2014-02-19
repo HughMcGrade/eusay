21b5971fd02e6afb1be6ce6ef7cce1c19a826983
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2014-02-19 17:43:57 +0000

    Make voting scripts reload page

--- eusay/static/js/voting.js
+++ eusay/static/js/voting.js
@@ -1,15 +1,16 @@
 function vote_up_proposal(proposal_id) {
-	$.get("/vote_proposal/up/" + proposal_id);
+	$.get("/vote_proposal/up/" + proposal_id, function(data){location.reload(true);});
 }
 
 function vote_down_proposal(proposal_id) {
-	$.get("/vote_proposal/down/" + proposal_id);
+	$.get("/vote_proposal/down/" + proposal_id, function(data){location.reload(true);});
+	
 }
 
 function vote_up_comment(proposal_id) {
-	$.get("/vote_comment/up/" + proposal_id);
+	$.get("/vote_comment/up/" + proposal_id, function(data){location.reload(true);});
 }
 
 function vote_down_comment(proposal_id) {
-	$.get("/vote_comment/down/" + proposal_id);
+	$.get("/vote_comment/down/" + proposal_id, function(data){location.reload(true);});
 }
