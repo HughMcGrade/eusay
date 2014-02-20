c39f63826b6d2f55b556c14184234331b5435096
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2014-02-20 21:10:17 +0000

    Move towards votes on comments

--- eusay/models.py
+++ eusay/models.py
@@ -14,6 +14,18 @@ class Comment (models.Model):
     user = models.ForeignKey("User")
     field = models.CharField(max_length=20, null=False)
     proposal = models.ForeignKey("Proposal", null=False)
+    
+    def _getVotes(self, isUp):
+        try:
+            return len(CommentVote.objects.all().filter(comment = self).filter(isVoteUp = isUp))
+        except Exception:
+            return 0
+    
+    def votesUp(self):
+        return self._getVotes(True)
+    
+    def votesDown(self):
+        return self._getVotes(False)
 
 class Vote (models.Model):
     id = models.AutoField(primary_key=True)
@@ -31,7 +43,7 @@ class Proposal (models.Model):
     
     def _getVotes(self, isUp):
         try:
-            return len(ProposalVote.objects.all().filter(proposal=self).filter(isVoteUp = isUp))
+            return len(ProposalVote.objects.all().filter(proposal = self).filter(isVoteUp = isUp))
         except Exception:
             return 0
     
--- eusay/static/js/voting.js
+++ eusay/static/js/voting.js
@@ -46,19 +46,79 @@ function set_proposal_votes_div(proposal_id, vote_div) {
 	});	
 }
 
-function vote_up_comment(comment_id) {
-	$.get("/vote_comment/up/" + comment_id, function(data){location.reload(true);});
+function vote_up_comment(comment_id, vote_div) {
+	//$.get("/vote_comment/up/" + comment_id, function(data){location.reload(true);});
+	$.ajax({
+		type : "get",
+		url : "/vote_comment/up/" + comment_id,
+		success: function (data) {
+			//alert(data);
+			//console.log(data);
+			$(vote_div).html(data);
+		},
+		error: function (data) {
+			alert(data.responseText);
+			console.log(data);
+		}
+	});	
 }
 
-function vote_down_comment(comment_id) {
-	$.get("/vote_comment/down/" + comment_id, function(data){location.reload(true);});
+function vote_down_comment(comment_id, vote_div) {
+	//$.get("/vote_comment/down/" + comment_id, function(data){location.reload(true);});
+	$.ajax({
+		type : "get",
+		url : "/vote_comment/down/" + comment_id,
+		success: function (data) {
+			//alert(data);
+			//console.log(data);
+			$(vote_div).html(data);
+		},
+		error: function (data) {
+			alert(data.responseText);
+			console.log(data);
+		}
+	});
+}
+
+function set_comment_votes_div(comment_id, vote_div) {
+	$.ajax({
+		type : "get",
+		url : "/vote_comment/get/" + comment_id,
+		success: function (data) {
+			//alert(data);
+			//console.log(data);
+			$(vote_div).html(data);
+		},
+		error: function (data) {
+			alert(data.responseText);
+			console.log(data);
+		}
+	});
 }
 
 function append_new_comment(div) {
 	$(div).append("New comment!")
 }
 
-function post_comment(form, proposal_id, field, user_sid, comment_div) {
+/*function post_comment(comment_text, proposal_id, field, user_sid, comment_div) {
+    $.ajax({
+        type: "post",
+        url: "post_comment/" + proposal_id + "/" + field,
+        data: { 'text' : comment_text },
+        success: function (data) {
+        	console.log(data);
+        	alert(data);
+            $(comment_div).html(data);
+        },
+        error: function(data) {
+        	console.log(data.responseText);
+        	alert(data);
+            $(comment_div).html("Couldn't retrieve comments");
+        }
+    });
+}*/
+function post_comment(form, comment_div) {
+
     var frm = $(form);
     console.log("hello");
     frm.submit(function () {
@@ -74,11 +134,37 @@ function post_comment(form, proposal_id, field, user_sid, comment_div) {
             error: function(data) {
             	console.log(data.responseText);
             	alert(data);
-                $(comment_div).html("Something went wrong!");
+                $(comment_div).html("Couldn't retrieve comments");
             }
         });
         return false;
     });
+}
+
+function set_proposal_field_comments_div_and_count(proposal_id, field, comment_div, comment_count_container) {
+      $.ajax({
+          type: 'get',
+          url: "/get_comments/" + proposal_id + "/" + field,
+            success: function (data) {
+                $(comment_div).html(data);
+            },
+            error: function(data) {
+            	console.log(data);
+                $(comment_div).html("Couldn't retrieve comments");
+            }
+      });
+      $.ajax({
+          type: 'get',
+          url: "/get_comments_count/" + proposal_id + "/" + field,
+            success: function (data) {
+            	console.log(data);
+                $(comment_count_container).html(data);
+            },
+            error: function(data) {
+            	console.log(data);
+                $(comment_count_container).html("Comments");
+            }
+      });
 }
 
 /*function post_comment(proposal_id, field, text, user_sid, comment_div) {
