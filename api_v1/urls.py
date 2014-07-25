66f2d155fd9bde21707b13bfe5400576daedd289
hecka_tao <tao@btao.org>
2014-07-25 21:53:11 +0100

    moved api urls to api_v1 app, & added comments to main urls.py file

---
+++ api_v1/urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls import patterns, url, include
+import rest_framework
+
+import api_v1.views as api_v1_views
+
+
+urlpatterns = patterns("",
+    url(
+        r'^proposals/$',
+        api_v1_views.ProposalList.as_view(),
+    ),
+    url(
+        r'^proposals/(?P<id>\d+)/$',
+        api_v1_views.ProposalDetail.as_view(),
+    ),
+    url(
+        r'^proposals/(?P<id>\d+)/comments/$',
+        api_v1_views.CommentList.as_view()
+    ),
+    # In templates, use {% url "api_v1:similarproposals" proposal.id %}
+    url(
+        r'^proposals/(?P<id>\d+)/similar/$',
+        api_v1_views.SimilarProposals.as_view(),
+        name="similarproposals"
+    ),
+    url(
+        r'^comments/(?P<id>\d+)/$',
+        api_v1_views.CommentDetail.as_view()
+    ),
+    url(
+        r'^search',
+        api_v1_views.SearchResults.as_view()
+    ),
+    url(
+        r'^auth/',
+        include('rest_framework.urls',
+                namespace='rest_framework')
+    ),
+)
--- eusay/urls.py
+++ eusay/urls.py
@@ -1,47 +1,52 @@
 from django.conf.urls import patterns, include, url
-import rest_framework
 from django.contrib import admin
 
 from eusay import views as eusay_views
-from api_v1 import views as api_v1_views
 from search import views as search_views
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'eusay.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    # API v1
+    url(r'^api/v1/', include("api_v1.urls", namespace="api_v1")),
 
-    url(r'^api/v1/proposals/$', api_v1_views.ProposalList.as_view()),
-    url(r'^api/v1/proposals/(?P<id>\d+)/$', api_v1_views.ProposalDetail.as_view()),
-    url(r'^api/v1/proposals/(?P<id>\d+)/comments/$', api_v1_views.CommentList.as_view()),
-    url(r'^api/v1/proposals/(?P<id>\d+)/similar/$', api_v1_views.SimilarProposals.as_view()),
-    url(r'^api/v1/comments/(?P<id>\d+)/$', api_v1_views.CommentDetail.as_view()),
-    url(r'^api/v1/search', api_v1_views.SearchResults.as_view()),
-    url(r'^api/v1/auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # Django admin
     url(r'^admin/', include(admin.site.urls)),
+
+    # Proposals
+    url(r'^$', eusay_views.index),  # Front page
+    url(r'^submit/', eusay_views.submit),
+    url(r'^proposal/(?P<proposalId>\d+)/(?P<slug>[\w-]*)',
+        eusay_views.proposal,
+        name="proposal"),
+
+    # Tags
+    url(r'^tag/(?P<tagId>\d+)/(?P<slug>[\w-]*)', eusay_views.tag, name="tag"),
+
+    # Profiles
+    url(r'^user/(?P<slug>[\w-]+)', eusay_views.profile, name="user"),
+
+    # Moderation
+    url(r'^comment_hides', eusay_views.comment_hides),
+    url(r'^proposal_hides', eusay_views.proposal_hides),
+    url(r'^moderator_panel', eusay_views.moderator_panel),
+    url(r'^search/', search_views.search),
+
+    # Action URLs
     url(r'^add_user/', eusay_views.add_user), # TODO: remove this, maybe?
     url(r'^get_users/', eusay_views.get_users),  # TODO: remove this, since it's for debugging
-    url(r'^$', eusay_views.index),
-    url(r'^submit/', eusay_views.submit),
-    url(r'^proposal/(?P<proposalId>\d+)/(?P<slug>[\w-]*)', eusay_views.proposal,
-        name="proposal"),
-    url(r'^tag/(?P<tagId>\d+)/(?P<slug>[\w-]*)', eusay_views.tag, name="tag"),
-    url(r'^user/(?P<slug>[\w-]+)', eusay_views.profile, name="user"),
     url(r'^vote_proposal/(?P<vote_request_type>(up|down|get))/(?P<proposal_id>\d+)', eusay_views.vote_proposal),
     url(r'^vote_comment/(?P<vote_request_type>(up|down|get))/(?P<comment_id>\d+)', eusay_views.vote_comment),
     url(r'^get_comments/(?P<proposal_id>\d+)/(?P<reply_to>\d*)', eusay_views.get_comments),
-    url(r'^about/', eusay_views.about),
-    url(r'^hide_comment/(?P<comment_id>\d+)', eusay_views.hide_comment),
-    url(r'^hide_proposal/(?P<proposal_id>\d+)', eusay_views.hide_proposal),
-    url(r'^comment_hides', eusay_views.comment_hides),
-    url(r'^proposal_hides', eusay_views.proposal_hides),
     url(r'^report_comment/(?P<comment_id>\d+)', eusay_views.report_comment),
     url(r'^report_proposal/(?P<proposal_id>\d+)', eusay_views.report_proposal),
-    url(r'^moderator_panel', eusay_views.moderator_panel),
     url(r'^make_mod', eusay_views.make_mod),
-    url(r'^search/', search_views.search),
-    url(r'^remove_comment/(?P<comment_id>\d+)', eusay_views.remove_comment),
     url(r'^get_messages/', eusay_views.get_messages),
+    url(r'^hide_comment/(?P<comment_id>\d+)', eusay_views.hide_comment),
+    url(r'^hide_proposal/(?P<proposal_id>\d+)', eusay_views.hide_proposal),
+    url(r'^remove_comment/(?P<comment_id>\d+)', eusay_views.remove_comment),
+
+    # Other
+    url(r'^about/', eusay_views.about),
+
 )
