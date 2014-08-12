1353b8fe4f9947b68af463d60476e7409f5ad57f
hecka_tao <tao@btao.org>
2014-08-12 12:56:15 +0100

    add utils to add proposals and comments

--- eusay/utils.py
+++ eusay/utils.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 from django.template.defaultfilters import slugify
 from django.conf import settings
@@ -23,21 +24,129 @@ def better_slugify(text, **kwargs):
             return slug
 
 
+def add_users(amount):
+    """
+    Add a bunch of users.
+    :param amount: number of users to be added
+    :return: True if successful, False otherwise
+    """
+    from .models import User
+
+    names = ['Tonja','Kaley','Bo','Tobias','Jacqui','Lorena','Isaac','Adriene','Tuan','Shanon','Georgette','Chas','Yuonne','Michelina','Juliana','Odell','Juliet','Carli','Asha','Pearl','Kamala','Rubie','Elmer','Taren','Salley','Raymonde','Shelba','Alison','Wilburn','Katy','Denyse','Rosemary','Brooke','Carson','Tashina','Kristi','Aline','Yevette','Eden','Christoper','Juana','Marcie','Wendell','Vonda','Dania','Sheron','Meta','Frank','Thad','Cherise']
+    generate_name = lambda: names[round((random.random() * 100) % 50) - 1]
+
+    def increment_sid():
+        last_user = User.objects.latest("createdAt")
+        last_sid_num = int(last_user.sid[1:])
+        new_sid_num = last_sid_num + 1
+        new_sid = "s{}".format(str(new_sid_num))
+        return new_sid
+
+    # If no users exist, add one
+    if User.objects.all().count() == 0:
+        User.objects.get_or_create(sid="s1",
+                                   name=(generate_name() + "1"))
+        amount -= 1
+
+    start_num = User.objects.all().count() + 1
+
+    i = start_num
+    while i <= start_num + amount:
+        name = generate_name() + str(i)
+        sid = increment_sid()
+        if not User.objects.filter(sid=sid).exists():
+            User.objects.create(sid=sid,
+                                name=name)
+            i += 1
+
+    return True
+
+
 def add_proposals(amount):
     """
     Add a bunch of (very!) generic proposals
+    :param amount: number of proposals to be added
+    :return: True if successful, False otherwise
     """
-    from .models import User, Proposal
-    if not amount:
-        raise Exception
 
-    start_num = max(User.objects.all().count(), Proposal.objects.all().count())
+    from .models import Proposal, User
+    titles = [
+        "Praesent commodo. Cursus magna, vel scelerisque!",
+        "Donec ullamcorper nulla non metus auctor fringilla!",
+        "Maecenas sed diam eget risus varius blandit non magna.",
+        "Etiam porta sem malesuada magna mollis euismod.",
+        "Cras mattis consectetur purus sit amet fermentum!",
+        "Praesent commodo cursus magna, vel scelerisque et.",
+        "Maecenas sed diam eget risus varius non magna.",
+        "Integer posuere erat a ante venenatis dapibus."
+        "Ipsum Etiam Justo Lorem Ultricies",
+        "Ridiculus Fringilla",
+        "Ornare Fusce Euismod!"
+    ]
 
-    for i in range(start_num + 1, start_num + amount + 1):
-        user = User.objects.create(sid="s" + str(i), name=str(i))
-        Proposal.objects.create(title=str(i),
-                                text="Proposal by " + user.name,
+    bodies = [
+        "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Vestibulum id ligula porta felis euismod semper. Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.",
+        "Cras mattis consectetur purus sit amet fermentum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras mattis consectetur purus sit amet fermentum. Donec sed odio dui.",
+        "Etiam porta sem malesuada magna mollis euismod. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Donec id elit non mi porta gravida at eget metus. Donec sed odio dui."
+    ]
+
+    get_title = lambda: random.choice(titles)
+    get_body = lambda: random.choice(bodies)
+
+    # If no users exist, add one
+    if User.objects.all().count() == 0:
+        add_users(1)
+
+    start_num = Proposal.objects.all().count() + 1
+
+    for i in range(start_num, start_num + amount + 1):
+        user = random.choice(User.objects.all())
+        Proposal.objects.create(title=get_title(),
+                                text=get_body(),
                                 user=user)
+    return True
+
+
+def add_comments(amount):
+    """
+    Add a bunch of top-level and reply comments to the latest proposal
+    :param amount: number of comments to be added
+    :param proposal_id: proposal to add comments to
+    :return: True if successful, False otherwise
+    """
+    from .models import Proposal, User, Comment
+    comments = [
+        "Praesent commodo. Cursus magna, vel scelerisque!",
+        "Donec ullamcorper nulla non metus auctor fringilla!",
+        "Maecenas sed diam eget risus varius blandit non magna.",
+        "Etiam porta sem malesuada magna mollis euismod.",
+        "Cras mattis consectetur purus sit amet fermentum!",
+        "Praesent commodo cursus magna, vel scelerisque et.",
+        "Maecenas sed diam eget risus varius non magna.",
+        "Integer posuere erat a ante venenatis dapibus."
+        "Ipsum Etiam Justo Lorem Ultricies",
+        "Ridiculus Fringilla",
+        "Ornare Fusce Euismod!"
+    ]
+
+    proposal = Proposal.objects.latest("createdAt")
+
+    for i in range(amount + 1):
+        user = random.choice(User.objects.all())
+        top_level_comments = Comment.objects.filter(proposal=proposal,
+                                                    replyTo=None)
+        if random.random() > 0.5 and top_level_comments.count() > 0:
+            reply_to = random.choice(Comment.objects.filter(proposal=proposal))
+            Comment.objects.create(text=random.choice(comments),
+                                   user=user,
+                                   proposal=proposal,
+                                   replyTo=reply_to)
+        else:
+            Comment.objects.create(text=random.choice(comments),
+                                   user=user,
+                                   proposal=proposal)
+
+    return True
 
 
 def to_queryset(searchqueryset):
