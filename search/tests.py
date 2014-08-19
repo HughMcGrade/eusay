5351b87978504a34711d09391bd34c9cdaeb0c56
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2014-08-19 15:36:44 +0100

    Fix some issues with search tests but proposal search still fails

--- search/tests.py
+++ search/tests.py
@@ -9,11 +9,11 @@ class SearchTest(TestCase):
         """
         Create a user, tag and proposal for these tests
         """
-        test_user = User.objects.create(sid="s1", name="Test User")
+        test_user = User.objects.create(sid="s1", username="Test User")
         test_tag = Tag.objects.create(name="Test tag")
         test_proposal = Proposal.objects.create(title="Test Proposal",
                                                 text="Test text",
-                                                proposer=test_user)
+                                                user=test_user)
         test_proposal.tags.add(test_tag)
 
     def test_search_in_title(self):
@@ -23,8 +23,9 @@ class SearchTest(TestCase):
         test_proposal = Proposal.objects.get(title="Test Proposal")
         response = self.client.get('/search/?q=proposal')
         self.assertEqual(response.status_code, 200)
+        self.assertIn('results', response.context)
         self.assertIn(test_proposal,
-                      [p.object for p in response.context['results']])
+                      [p.object for p in response.context['results'] if p])
 
     def test_search_in_text(self):
         """
