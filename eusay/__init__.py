8e54a9bd4c516fb31167fb7109aee3f66e8985bd
Hugh McGrade <hugh_mcgrade@hotmail.co.uk>
2014-08-20 12:17:45 +0100

    Remove old eusay app

--- eusay/__init__.py
+++
@@ -1,3 +0,0 @@
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import celery as celery_app
--- eusay/admin.py
+++
@@ -1,3 +0,0 @@
-from django import forms
-from django.contrib import admin
-from eusay.models import Tag, Proposal
