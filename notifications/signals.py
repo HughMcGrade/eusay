1f08a35ae19c73ffcb1db9b78edf7e8c4e820a72
hecka_tao <tao@btao.org>
2014-08-21 19:26:13 +0100

    remove notifications for a user's own actions

--- notifications/signals.py
+++ notifications/signals.py
@@ -8,10 +8,11 @@ from .models import Notification
 
 @receiver(post_save, sender=Response)
 def notify_of_response(created, **kwargs):
-    if created:
-        response = kwargs.get("instance")
-        voters = response.proposal.get_voters()
-        proposer = response.proposal.user
+    response = kwargs.get("instance")
+    voters = response.proposal.get_voters()
+    proposer = response.proposal.user
+    # Don't send a notification if the response was written by the proposer
+    if created and response.user != proposer:
         # Make sure the proposer doesn't get notified twice
         if proposer in voters:
             voters = voters.exclude(id=proposer.id)
@@ -28,9 +29,10 @@ def notify_of_response(created, **kwargs):
 
 @receiver(post_save, sender=Comment)
 def notify_proposer_of_comment(created, **kwargs):
-    if created:
-        comment = kwargs.get("instance")
-        recipient = comment.proposal.user
+    comment = kwargs.get("instance")
+    recipient = comment.proposal.user
+        # Don't send a notification if a user comments on their own proposal
+    if created and comment.user != recipient:
         # Don't send this notification if someone replies to a proposer's own
         # comment on their own proposal, since they'll get a notification of a
         # reply anyway.
@@ -47,10 +49,12 @@ def notify_proposer_of_comment(created, **kwargs):
 
 @receiver(post_save, sender=Comment)
 def notify_commenter_of_reply(created, **kwargs):
-    if created:
-        comment = kwargs.get("instance")
-        if comment.replyTo is not None:
-            recipient = comment.replyTo.user
+    comment = kwargs.get("instance")
+    # Only send a notification if it's a reply
+    if created and comment.replyTo:
+        recipient = comment.replyTo.user
+        # Don't send a notification to the user who submitted the reply
+        if recipient != comment.user:
             type = "comment_reply"
             content = comment.replyTo
             Notification.objects.create(recipient=recipient,
@@ -62,6 +66,8 @@ def notify_commenter_of_reply(created, **kwargs):
 def notify_commenter_of_reply_in_thread(created, **kwargs):
     comment = kwargs.get("instance")
     if created and comment.replyTo:
+        # Don't notify the parent comment's submitter if they reply to their
+        # own comment
         recipients = [reply.user for reply in comment.replyTo.get_replies()
                       if not reply.user == comment.user]
         type = "comment_reply"
