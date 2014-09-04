69c915abb8186cda5e3edb27a421781976870785
Tao Bojlen <tao@btao.org>
2014-09-04 14:19:50 +0100

    standardize look of search results page

--- search/views.py
+++ search/views.py
@@ -4,8 +4,22 @@ from haystack.query import SearchQuerySet
 
 
 def search(request):
+    template = "search/search.html"
+    proposals_template = "proposal_list.html"
+    context = {
+        "proposals_template": proposals_template,
+    }
+
     if request.method == "GET":
         if "q" in request.GET:
             query = str(request.GET.get("q"))
-            results = SearchQuerySet().all().filter(content=query).load_all()
-    return render(request, "search/search.html", {"results": results})
+            results = [r.object for r in
+                       SearchQuerySet().filter(content=query).load_all()]
+            context["query"] = query
+            context["proposals"] = results
+
+    if request.is_ajax() and 'page' in request.GET:
+        # AJAX request for pagination
+        template = proposals_template
+
+    return render(request, template, context)
